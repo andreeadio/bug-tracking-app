07fb1ec2c4362566f129d3b47f6ad39ba5289a86
andreeanafornita <nafornitadaniela21@stud.ase.ro>
2023-12-20 22:17:00 +0200

    Added projectStudent entity and also projectStudent router along with a get and a post method. Also added on server.js all necessary changes in order to start the port on a server, initialize the routes and also the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
-//server
-const express = require('express')
+const express=require('express')
+const app=express()//aceste 2 linii de cod ne ajuta sa facem request pe api si sa initializam serverul
+const db=require('./models')//asa creem conexiunea dintre ORM si baza de date din mysql
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+//Routers
+const projectstudent=require('./routes/projectStudentRouter')
+app.use("/projectstudent",projectstudent)
 
-app.listen(8080, () => {
-    console.log('Server is running on 8080')
-})
+db.sequelize.sync().then(()=>{
+//start api
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{//atunci cand serverul porneste pe acest port
+    console.log(`Server running on port ${port}`);//mesaj de testare
+    });
+});
