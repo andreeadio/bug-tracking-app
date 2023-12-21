a016ad7f7244edb8e8816452287f3a4d4126ab1b
alexandramaxim <maximalexandra21@stud.ase.ro>
2023-12-21 14:54:29 +0200

    Created route and entity for general Users

--- backend/entitites/Users.js
+++ backend/entitites/Users.js
@@ -1,40 +1,21 @@
-class User {
-    constructor(email, role) {
-      this.email = email;
-      this.role = role;
-      this.projects = [];
-      this.allocatedBug = null;
-    }
-  
-    registerProject(description, repository, projectTeam) {
-      // Implementation for registering a project
-    }
-  
-    registerAsTester(project) {
-      // Implementation for registering as a tester for a project
-    }
-  
-    registerBug(project, severity, description, commitLink) {
-      // Implementation for registering a bug
-    }
-  
-    viewBugs(project) {
-      // Implementation for viewing bugs in a project
-    }
-  
-    allocateBug(bug) {
-      // Implementation for allocating a bug to the user
-    }
-  
-    solveBug(bug, commitLink) {
-      // Implementation for solving a bug
-    }
-  
-    addStatusToSolution(bug, status, commitLink) {
-      // Implementation for adding a status to the bug solution
-    }
-  
-    hasPermission(action, project) {
-      // Implementation for checking permissions based on user role
-    }
+module.exports=(sequelize, DataTypes)=>{
+  const Users=sequelize.define("Users",{
+  Id:{
+      type:DataTypes.INTEGER,
+      allowNull:false, 
+  },
+ name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+  },
+  email:{
+      type:DataTypes.STRING,  
+      allowNull:false,
+  },
+ password:{
+      type:DataTypes.STRING,
+      allowNull:true,
   }
+  });
+  return Users
+  };
