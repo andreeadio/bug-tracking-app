2100876ef2975ec356e9629056816e907c663278
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2023-12-23 01:37:33 +0200

    created Bugs entity model

---
+++ backend/models/Bugs.js
@@ -0,0 +1,36 @@
+
+module.exports = (sequelize, DataTypes) => {
+
+    const Bugs = sequelize.define("bugs", {
+        //define the attributes
+        // severitate, prioritate, descriere, link commit
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        severityFlag: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        priority: {
+            type: DataTypes.ENUM('Low', 'Medium', 'High'),
+            allowNull: false,
+            defaultValue: 'Medium'
+        },
+        commitLink: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    })
+
+    return Bugs
+}
