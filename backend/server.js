52691282d170503865c631f52af59c9dd5108d61
alexandramaxim <maximalexandra21@stud.ase.ro>
2023-12-26 10:32:10 +0200

    Installed axios dependency

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,6 @@ const app = express()//aceste 2 linii de cod ne ajuta sa facem request pe api si
 const db = require('./models')//asa creem conexiunea dintre ORM si baza de date din mysql
 app.use(express.json())
 
-//Routes
-const projectStudent = require('./routes/projectStudentRouter')
-app.use("/projectstudent", projectStudent)
-
-const projectUser = require('./routes/userRouter')
-app.use("/projectuser", projectUser)
-
-const bugsRouter = require('./routes/bugRouter')
-app.use("/bugs", bugsRouter)
-
 db.sequelize.sync().then(() => {
     //start api
     const port = process.env.PORT || 8080
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(()=>{
+    axios.get("")
+  }, [])
   return (
     <div className="App">
     
