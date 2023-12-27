0e74348fb696b78ca511e2d39aa380a9a428cf60
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2023-12-28 00:28:41 +0200

    added cors dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@
 
 const express = require('express')
 const app = express()//aceste 2 linii de cod ne ajuta sa facem request pe api si sa initializam serverul
+//import cors
+const cors = require('cors')
+
 const db = require('./models')//asa creem conexiunea dintre ORM si baza de date din mysql
 app.use(express.json())
-
+app.use(cors())
 
 //routes
-const accountRoutes=require('./routes/accountRoutes')
-app.use("/auth",accountRoutes) 
+const accountRoutes = require('./routes/accountRoutes')
+app.use("/auth", accountRoutes)
+
 //bug router
 const bugsRouter = require('./routes/bugRouter')
-app.use("/bugs", bugsRouter)  
+app.use("/bugs", bugsRouter)
 
 
 db.sequelize.sync().then(() => {
