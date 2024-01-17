07a6a7814c0cd41cd7264dd9a6d9a7c68203a434
alexandramaxim <maximalexandra21@stud.ase.ro>
2024-01-17 11:10:48 +0200

    Added functionalities for the addProject btn

--- frontend/src/components/MemberProjectPage.js
+++ frontend/src/components/MemberProjectPage.js
@@ -2,16 +2,18 @@ import "primereact/resources/themes/lara-dark-pink/theme.css" //theme
 import "primereact/resources/primereact.min.css" //core css
 
 import React, { useState, useEffect } from 'react';
-import { InputText } from 'primereact/inputtext';
+//import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { ListBox } from 'primereact/listbox';
 import axios from 'axios';
 import { SERVER } from '../config/global';
+import ProjectDialog from './ProjectDialog';
 
 const MemberProjectPage = () => {
   const [projects, setProjects] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  //const [searchText, setSearchText] = useState('');
+  const [showProjectDialog, setShowProjectDialog] = useState(false);
 
  
   const fetchProjects = async () => {
@@ -29,7 +31,12 @@ const MemberProjectPage = () => {
 
   const handleAddProject = () => {
     // Add logic for adding a new project (e.g., show a modal, navigate to a new page)
-    console.log('Add Project clicked');
+    setShowProjectDialog(true);
+  };
+
+  const handleProjectDialogHide = () => {
+    // This function is called when the ProjectDialog is closed or hidden
+    setShowProjectDialog(false);
   };
 
   return (
@@ -49,6 +56,9 @@ const MemberProjectPage = () => {
 
       <div className="add-project-button">
         <Button label="Add New Project" icon="pi pi-plus" onClick={handleAddProject} />
+
+        {/* Render the ProjectDialog conditionally based on showProjectDialog state */}
+      <ProjectDialog visible={showProjectDialog}  onHide={handleProjectDialogHide} onProjectAdded={fetchProjects} />
 
       </div>
     </div>
--- frontend/src/components/ProjectAddForm.js
+++ frontend/src/components/ProjectAddForm.js
@@ -1,19 +1,153 @@
-import "primereact/resources/themes/lara-dark-pink/theme.css" //theme
-import "primereact/resources/primereact.min.css" //core css
+// import "primereact/resources/themes/lara-dark-pink/theme.css" //theme
+// import "primereact/resources/primereact.min.css" //core css
 
+
+// import { useState } from "react";
+// import axios from "axios";
+// import { SERVER } from '../config/global';
+
+
+// //import primereact components
+// import { Button } from 'primereact/button';
+// import { InputText } from 'primereact/inputtext';
+
+// const ProjectAddForm = ({ onProjectAdded }) => {
+//     const [project, setProjects] = useState(
+//         {
+//             link: '',
+//             teamID: '',
+//             //can add more if logic changes      
+//         }
+//     )
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+
+//         try {
+//             //make the post request
+//             await axios.post(`${SERVER}/projects`, project);
+//             // Assuming the API call is successful, reset the form and notify the parent component
+//             setProjects({
+//                 link: '',
+//                 teamID: ''
+//             });
+//             onProjectAdded();
+
+//             //display message
+//             window.alert('Project successfully added!');
+//         } catch (error) {
+//             console.error('Error adding project:', error);
+//             window.alert('Error when adding a project');
+
+//         }
+//     }
+
+//     return (
+//         <div>
+
+//             <form onSubmit={handleSubmit}>
+
+//                 <div className="p-fluid">
+
+//                     <div className="p-field">
+//                         <label htmlFor="link">Link</label>
+//                         <InputText id="link" name="link" value={project.link}
+//                             required
+//                         />
+//                     </div>
+
+//                     <div className="p-field">
+//                         <label htmlFor="teamID">Team</label>
+//                         <InputText
+//                             id="teamID" name="teamID" value={project.teamID}
+//                             required
+//                         />
+//                     </div>   
+//                 </div>
+
+//                 <Button type="submit" label="Submit" onClick={handleSubmit} />
+//             </form>
+//         </div>
+//     )
+
+// }
+
+// export default ProjectAddForm
+
+import "primereact/resources/themes/lara-dark-pink/theme.css"; // theme
+import "primereact/resources/primereact.min.css"; // core css
 
 import { useState } from "react";
 import axios from "axios";
 import { SERVER } from '../config/global';
 
-
-//import primereact components
+// import primereact components
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { ToggleButton } from 'primereact/togglebutton';
-import { Dropdown } from 'primereact/dropdown';
 
-const ProjectAddForm = ({ onProjectAdded }) => {}
+const ProjectAddForm = ({ onProjectAdded }) => {
+    const [project, setProject] = useState({
+        link: '',
+        teamID: '',
+    });
 
-export default ProjectAddForm
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProject((prevProject) => ({ ...prevProject, [name]: value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // make the post request
+            await axios.post(`${SERVER}/projects`, project);
+            // Assuming the API call is successful, reset the form and notify the parent component
+            setProject({
+                link: '',
+                teamID: '',
+            });
+            onProjectAdded();
+
+            // display message
+            window.alert('Project successfully added!');
+        } catch (error) {
+            console.error('Error adding project:', error);
+            window.alert('Error when adding a project');
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="p-fluid">
+                    <div className="p-field">
+                        <label htmlFor="link">Link</label>
+                        <InputText
+                            id="link"
+                            name="link"
+                            value={project.link}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="p-field">
+                        <label htmlFor="teamID">Team</label>
+                        <InputText
+                            id="teamID"
+                            name="teamID"
+                            value={project.teamID}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                </div>
+
+                <Button type="submit" label="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default ProjectAddForm;
